b8bed7bb2413fbfa1d6b79085a1a0b7a3271aade
Martin <javaprogramer1985@gmail.com>
2022-10-18 09:29:59 -0500

    Pruebas del componente GitCardItem.jsx

--- src/components/GifCardItem.jsx
+++ src/components/GifCardItem.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { PropTypes } from 'prop-types';
 
 export const GifCardItem = ({title,url,id}) => {
   return (  
@@ -7,4 +8,9 @@ export const GifCardItem = ({title,url,id}) => {
       <p>{title}</p>      
     </div>
   )
+}
+
+GifCardItem.propTypes = {
+  title : PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
